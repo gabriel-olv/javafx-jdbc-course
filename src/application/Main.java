d6f2540a88e569f4417f93ab479eb97861b3a1c0
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-08-22 00:13:45 -0300

    About and DepartmentList view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene scene;
+	
 	@Override
 	public void start(Stage stage) {
 		try {
@@ -21,14 +24,17 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			scrollPane.borderProperty().set(Border.EMPTY);
 			
-			Scene scene = new Scene(scrollPane);
-			
+			scene = new Scene(scrollPane);
 			stage.setTitle("Sample JavaFX application");
 			stage.setScene(scene);
 			stage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static Scene getScene() {
+		return scene;
 	}
 	
 	public static void main(String[] args) {
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,37 +1,65 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.utils.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
+	}
+
+	private synchronized void loadView(String path) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
+			VBox vBox = fxmlLoader.load();
+			
+			Scene mainScene = Main.getScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			MenuBar mainMenu = (MenuBar) mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(vBox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
